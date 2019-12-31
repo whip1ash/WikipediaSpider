2ebc6f7ec05dbd6e82c43290bea68d8cac810240
2333 whip1ash <usergeng@gmail.com>
2019-12-31 11:32:09 +0800

    fix some bugs

---
+++ CustomSetting/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+'''
+@author: whip1ash
+@contact: security@whip1ash.cn
+@software: pycharm 
+@file: __init__.py.py
+@time: 2019/12/31 11:30
+@desc:
+'''
---
+++ CustomSetting/setting.py
@@ -0,0 +1,1 @@
+custom_proxy_setting = 'https://127.0.0.1:1087'
